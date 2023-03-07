f7a17d527449672b1fba29abce474c0d48e90dc0
sahoooii <saho.chu@gmail.com>
2023-03-07 18:11:11 +0900

    created .gitignore and installed howler

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,35 @@
 import { cardGenerator } from './CardGenerate.js';
 
+cardGenerator();
+
+
+function GameReady() {
+	let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+	let cards = Array.from(document.getElementsByClassName('card'));
+
+	// start game
+	overlays.forEach((overlay) => {
+		overlay.addEventListener('click', () => {
+			overlay.classList.remove('visible');
+			// game.startGame();
+		});
+	});
+	// card
+	cards.forEach((card) => {
+		card.addEventListener('click', () => {
+			// game.flipCard(card)
+		});
+	});
+}
+
 const playerLiveCount = document.getElementById('playerLiveCount');
 let playerLives = 6;
 
 // playerLives
 playerLiveCount.textContent = playerLives;
 
-
-cardGenerator();
+if (document.readyState == 'loading') {
+	document.addEventListener('DOMContentLoaded', GameReady);
+} else {
+	GameReady();
+}
