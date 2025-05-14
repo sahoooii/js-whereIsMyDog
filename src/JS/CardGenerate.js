d54f4abc6be76b8d78f79706bd90d5ca6f6ee1da
sahoooii <saho.chu@gmail.com>
2025-05-14 16:15:12 +0900

    Refactor folder structure and fix asset loading issues with Webpack
    - Moved CSS files to src/styles to resolve CSS loader errors
    - Relocated images and cursor assets to match Webpack handling rules
    - Adjusted paths in CSS and JS for compatibility with build output
    - Configured CopyWebpackPlugin to copy static assets to dist/

--- src/JS/CardGenerate.js
+++ src/JS/CardGenerate.js
@@ -3,47 +3,51 @@ import getCardData from './getCardData';
 const section = document.querySelector('section');
 
 //Generate html
+// <div class='card'>
+// 	<div class='card-back card-face'>
+// 		<img class='card-backImg' />
+// 	</div>
+// 	<div class='card-front card-face'>
+// 		<img class='card-value' alt='' />
+// 	</div>
+// </div>;
+const createCard = (item) => {
+	// Create elements
+	const card = document.createElement('div');
+	const cardBack = document.createElement('div');
+	const cardBackImg = document.createElement('img');
+	const cardFront = document.createElement('div');
+	const cardFrontImg = document.createElement('img');
+
+	//Add class to card
+	card.classList.add('card');
+	cardBack.classList.add('card-back', 'card-face');
+	cardBackImg.classList.add('card-backImg');
+	cardFront.classList.add('card-front', 'card-face');
+	cardFrontImg.classList.add('card-value');
+
+	//Add card img src and alt
+	cardBackImg.src = '../Images/cardBack.png';
+	cardBackImg.setAttribute('alt', 'backImg-pawBox');
+	cardFrontImg.src = item.imgSrc;
+	cardFrontImg.setAttribute('alt', item.name);
+
+	//Add elements
+	card.appendChild(cardBack);
+	cardBack.appendChild(cardBackImg);
+	card.appendChild(cardFront);
+	cardFront.appendChild(cardFrontImg);
+
+	return card;
+};
+
 export const cardGenerator = () => {
 	//Shuffle cards
 	const cardData = getCardData();
 	cardData.sort(() => Math.random() - 0.5);
 
 	cardData.forEach((item) => {
-		// Create elements
-		const card = document.createElement('div');
-		const cardBack = document.createElement('div');
-		const cardBackImg = document.createElement('img');
-		const cardFront = document.createElement('div');
-		const cardFrontImg = document.createElement('img');
-
-		//Add class
-		card.classList.add('card');
-		cardBack.classList.add('card-back', 'card-face');
-		cardBackImg.classList.add('card-backImg');
-		cardFront.classList.add('card-front', 'card-face');
-		cardFrontImg.classList.add('card-value');
-
-		//Add card img src and alt
-		cardBackImg.src = '../Images/cardBack.png';
-		cardBackImg.setAttribute('alt', 'backImg-pawBox');
-		cardFrontImg.src = item.imgSrc;
-		cardFrontImg.setAttribute('alt', item.name);
-
-		//Add elements
+		const card = createCard(item);
 		section.appendChild(card);
-		card.appendChild(cardBack);
-		cardBack.appendChild(cardBackImg);
-		card.appendChild(cardFront);
-		cardFront.appendChild(cardFrontImg);
-
-		// Created html inside of Section
-		// <div class='card'>
-		// 	<div class='card-back card-face'>
-		// 		<img class='card-backImg' />
-		// 	</div>
-		// 	<div class='card-front card-face'>
-		// 		<img class='card-value' alt='' />
-		// 	</div>
-		// </div>;
 	});
 };
